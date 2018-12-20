2d228878ae2672077ede3cf7e1755bf776207039
yaser omran <engineeryaseromran@gmail.com>
2018-12-20 18:37:58 +0200

    yaser-dell

--- app/src/main/java/com/example/plainolnotes3/MainActivity.java
+++ app/src/main/java/com/example/plainolnotes3/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.plainolnotes3;
 /*
 hi updated in github from yaser omran
-change from yaser-dell 2
+change from yaser-dell 3
 */
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
