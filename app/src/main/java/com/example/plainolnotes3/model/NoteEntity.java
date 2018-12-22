6a1fa355f2cfdb263380cb0e427684f1649ae654
yaser omran <engineeryaseromran@gmail.com>
2018-12-22 08:51:16 +0200

    yaser-dell

--- app/src/main/java/com/example/plainolnotes3/model/NoteEntity.java
+++ app/src/main/java/com/example/plainolnotes3/model/NoteEntity.java
@@ -1,4 +1,44 @@
 package com.example.plainolnotes3.model;
 
+import java.util.Date;
+
 public class NoteEntity {
+    private  int id;
+    private Date date;
+    private String text;
+
+    public NoteEntity(Date date, String text) {
+        this.date = date;
+        this.text = text;
+    }
+
+    public NoteEntity(int id, Date date, String text) {
+        this.id = id;
+        this.date = date;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
