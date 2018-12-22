1692332a063d451b03fd336e9a2ec4daf264e6e3
yaser omran <engineeryaseromran@gmail.com>
2018-12-22 18:48:49 +0200

    yaser-dell

--- app/src/main/java/com/example/plainolnotes3/MainActivity.java
+++ app/src/main/java/com/example/plainolnotes3/MainActivity.java
@@ -4,20 +4,33 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.plainolnotes3.database.NoteEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.recycler_view)      RecyclerView mRecyclerView;
+    private List<NoteEntity> notesData = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        ButterKnife.bind(this);
+        initRecyclerView();
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +40,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+private  void initRecyclerView(){
+        mRecyclerView.setHasFixedSize(true);
+    LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+    mRecyclerView.setLayoutManager(linearLayoutManager);
+}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
