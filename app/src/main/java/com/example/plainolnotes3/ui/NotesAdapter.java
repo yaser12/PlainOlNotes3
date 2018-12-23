6b1be86cc6298bf21108785462a48710e226399c
yaser omran <engineeryaseromran@gmail.com>
2018-12-23 19:48:35 +0200

    yaser-dell

---
+++ app/src/main/java/com/example/plainolnotes3/ui/NotesAdapter.java
@@ -0,0 +1,56 @@
+package com.example.plainolnotes3.ui;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.plainolnotes3.R;
+import com.example.plainolnotes3.database.NoteEntity;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class NotesAdapter extends RecyclerView.Adapter <NotesAdapter.ViewHolder>{
+    private final List<NoteEntity> mNotes;
+    private final Context mContext;
+
+    public NotesAdapter(List<NoteEntity> mNotes, Context mContext) {
+        this.mNotes = mNotes;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        View view=inflater.inflate(R.layout.not_list_item,parent,false);
+        return  new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final NoteEntity note=mNotes.get(position);
+        holder.mtTextView.setText(note.getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.note_text)         TextView mtTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+}
