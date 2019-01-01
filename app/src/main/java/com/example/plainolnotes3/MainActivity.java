b8342e21627fa003fbd31a6b198633f6467f921b
yaser omran <engineeryaseromran@gmail.com>
2019-01-01 13:17:21 +0200

    firsttry

--- app/src/main/java/com/example/plainolnotes3/MainActivity.java
+++ app/src/main/java/com/example/plainolnotes3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.plainolnotes3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,6 +22,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     private List<NoteEntity> notesData = new ArrayList<>();
     private NotesAdapter mAdapter;
+    @OnClick(R.id.fab) void fabClickHandler(){
+        Intent intent=new Intent(this,EditorActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,14 +43,7 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView=(RecyclerView)findViewById(R.id.recycler_view);
         ButterKnife.bind(this);
         initRecyclerView();
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         notesData.addAll(SampleData.getNotes());
         for (NoteEntity note :
                 notesData) {
