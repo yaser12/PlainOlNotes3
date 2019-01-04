bfff3c87a3187fb231eb97c3485fc8e31ff4bd33
yaser omran <engineeryaseromran@gmail.com>
2019-01-04 13:31:36 +0200

    public  abstract NoteDao noteDao();

--- app/src/main/java/com/example/plainolnotes3/database/AppDatabase.java
+++ app/src/main/java/com/example/plainolnotes3/database/AppDatabase.java
@@ -13,7 +13,7 @@ public abstract class AppDatabase extends RoomDatabase {
     public static final String DATABASE_NAME = "AppDatabase.dba";
     private static volatile AppDatabase instance;
     private static final Object LOCK = new Object();
-
+public  abstract NoteDao noteDao();
     public static AppDatabase getInstance(Context context) {
         if (instance != null) {
             synchronized (LOCK) {
