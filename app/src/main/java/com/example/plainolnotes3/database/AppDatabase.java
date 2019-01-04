66b5286346575e6b2a042638b20e491c8857cad8
yaser omran <engineeryaseromran@gmail.com>
2019-01-04 12:45:16 +0200

    فقط AppDatabase

---
+++ app/src/main/java/com/example/plainolnotes3/database/AppDatabase.java
@@ -0,0 +1,29 @@
+package com.example.plainolnotes3.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {NoteEntity.class},version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public static final String DATABASE_NAME= "AppDatabase.dba";
+    private static volatile AppDatabase instance;
+    private static final Object LOCK=new Object();
+
+    public static AppDatabase getInstance(Context context) {
+        if(instance!=null)
+        {
+            synchronized (LOCK)
+            {
+                if(instance!=null)
+                {
+                    instance= Room.databaseBuilder(context.getApplicationContext(),AppDatabase.class,DATABASE_NAME).build();
+                }
+
+
+            }
+        }
+        return instance;
+    }
+}
